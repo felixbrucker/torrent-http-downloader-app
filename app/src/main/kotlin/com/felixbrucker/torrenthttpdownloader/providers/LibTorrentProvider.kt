2ac81e12e90ee94cd0445f42e2e572ea841860b8
Felix Brucker <felix@felixbrucker.com>
2026-05-29 23:52:45 +0800

    Set auto managed on start and enable sequential downloads

--- app/src/main/kotlin/com/felixbrucker/torrenthttpdownloader/providers/LibTorrentProvider.kt
+++ app/src/main/kotlin/com/felixbrucker/torrenthttpdownloader/providers/LibTorrentProvider.kt
@@ -120,6 +120,9 @@ class LibTorrentProvider(
         val torrentHandle = sessionManager.find(Sha1Hash.parseHex(torrentId)) ?: throw Exception("Torrent not found")
         torrentHandle.swig().set_max_connections(defaultSessionSettings.connectionsLimitPerTorrent)
         torrentHandle.swig().set_max_uploads(defaultSessionSettings.uploadsLimitPerTorrent)
+        torrentHandle.flags = torrentHandle.flags
+            .or_(TorrentFlags.AUTO_MANAGED)
+            .or_(TorrentFlags.SEQUENTIAL_DOWNLOAD)
     }
 
     override suspend fun getTorrentInfo(id: String): ProviderTorrentInfo {
